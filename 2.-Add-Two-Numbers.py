79de1492d3c030155ff7acc982819f44934eca56
Wole Fabikun <wole359@gmail.com>
2023-10-06 22:48:52 -0400

    Linked List to List

--- 2.-Add-Two-Numbers.py
+++ 2.-Add-Two-Numbers.py
@@ -1,5 +1,6 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        #turn the linked lists into lists of char
         curr1 = l1
         arr1 =[]
         while curr1:
@@ -11,16 +12,18 @@ class Solution:
         while curr2:
             arr2.append(str(curr2.val))
             curr2 = curr2.next
-
+        
+        #reverse the lists
         arr1 = arr1[::-1]
         arr2 = arr2[::-1]
 
+        #join and turn to int then add and turn to str again so that we can reverse again
         one = int("".join(arr1))
         two = int("".join(arr2))
-
         ans = str(one + two)
         ans = ans[::-1]
 
+        #turn str into linked list, turn the char into int while you are creating nodes 
         head = linked = ListNode(int(ans[0])) 
 
         for i in range(1,len(ans)):
@@ -28,5 +31,3 @@ class Solution:
             linked = linked.next
             
         return head 
-              
-
